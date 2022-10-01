45358d64d083f1309fde35438a6bf76df8da0974
shadab <me.shadab19@gmail.com>
2022-10-01 17:02:34 +0530

    [Added: comparing line using Oops Concept ]

---
+++ src/com/Oops/LineComparisonMain.java
@@ -0,0 +1,13 @@
+package com.Oops;
+
+public class LineComparisonMain {
+
+	public static void main(String[] args) {
+		System.out.println("~~WELCOME TO LINE COMPARISON COMPUTATION PROGRAM~~");
+		LineComparison lc = new LineComparison();
+		lc.getLenght();
+		lc.compareLines();
+		
+	}
+
+}
